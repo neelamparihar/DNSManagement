2ef7d811108dd65c9d456d6a8a8decb5a1b51174
Neelam <pariharneelam376@gmail.com>
2024-04-01 00:25:41 +0530

    final build

---
+++ frontend/src/components/Layout/Header.jsx
@@ -0,0 +1,142 @@
+import logo from "../../assets/test1.png";
+import { IN } from "country-flag-icons/react/3x2";
+import { Link } from "react-router-dom";
+import { UserContext } from "../LoginPage/UserContext";
+import { useContext } from "react";
+const Header = () => {
+  const { user } = useContext(UserContext);
+  return (
+    <nav className="border-b border-gray-300 shadow-md">
+      <div className=" max-w-full mx-auto px-4 pt-2 flex justify-between items-center  ">
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="h-14 mx-6" />
+          <ul className="flex space-x-6">
+            <li className="nav-item">
+              {/* <Link to="/" className="nav-link"> */}
+              Home
+              {/* </Link> */}
+            </li>
+            <li className="nav-item">
+              {/* <Link to="/websites" className="nav-link"> */}
+              Websites
+              {/* </Link> */}
+            </li>
+            <li className="nav-item">
+              {/* <Link to="/emails" className="nav-link"> */}
+              Emails
+              {/* </Link> */}
+            </li>
+            <li className="nav-item">
+              {/* <Link to="/domains" className="nav-link"> */}
+              Domains
+              {/* </Link> */}
+            </li>
+            <li className="nav-item">
+              {/* <Link to="/vps" className="nav-link"> */}
+              VPS
+              {/* </Link> */}
+            </li>
+            <li className="nav-item">
+              {/* <Link to="/billing" className="nav-link"> */}
+              Billing
+              {/* </Link> */}
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center ">
+          <div className="flex items-center space-x-8">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-7"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </div>
+            <div>
+              <IN title="United States" className="h-6 rounded-sm" />
+            </div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-7"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
+                />
+              </svg>
+            </div>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-7"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46"
+                />
+              </svg>
+            </div>
+          </div>
+          <Link to="/account" className="ml-8 mr-2">
+            {user && user.name && user.email ? (
+              <div className="mt-6">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+                <span>Hi, {user.name}</span>
+              </div>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            )}
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
